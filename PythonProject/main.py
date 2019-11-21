fa921e0ffafb025b3e9ad5f17e0dc2a820528639
komakow <42356216+komakow@users.noreply.github.com>
2019-11-21 20:40:37 +0100

    Add photo and PyCharm prj
    Add some images to learn at them and PyCharm project with main.py

---
+++ PythonProject/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+#load a color image in greyscale
+
+img = cv.imread('../images/ludzie1.jpg', 1)
+b,g,r = cv.split(img)
+img2 = cv.merge([r,g,b])
+plt.subplot(121);plt.imshow(img) # expects distorted color
+plt.subplot(222);plt.imshow(img2) # expect true color
+plt.show()
