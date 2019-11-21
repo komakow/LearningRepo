dbb02caf3702a4e4aa718216012ff82c60e84153
komakow <42356216+komakow@users.noreply.github.com>
2019-11-21 19:52:22 +0100

    add openCV documentation
    OpenCV 4.1.1

---
+++ doc/4.1.1/search/enums_c.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['ncvmemorytype',['NCVMemoryType',['../d5/dc3/group__cudalegacy.html#gaeadd62ef0a866ec64405271f77ef0901',1,'NCV.hpp']]],
+  ['normalizationtype',['NormalizationType',['../d9/d37/classcv_1_1xfeatures2d_1_1DAISY.html#aaf3a347b21a1fd2eeb0b56cce5276151',1,'cv::xfeatures2d::DAISY']]],
+  ['normtypes',['NormTypes',['../d2/de8/group__core__array.html#gad12cefbcb5291cf958a85b4b67b6149f',1,'cv']]],
+  ['nppstbordertype',['NppStBorderType',['../d1/d0d/group__nppi.html#ga8b115979ee872d8e356c2f1f162d483c',1,'NPP_staging.hpp']]],
+  ['nppstinterpmode',['NppStInterpMode',['../d1/d0d/group__nppi.html#ga0bfe03940f062d1a975f748a6ce9357d',1,'NPP_staging.hpp']]],
+  ['nvidia_5fof_5fperf_5flevel',['NVIDIA_OF_PERF_LEVEL',['../dc/d9d/classcv_1_1cuda_1_1NvidiaOpticalFlow__1__0.html#a6ff2c6689c0b236d6d18226b9698883b',1,'cv::cuda::NvidiaOpticalFlow_1_0']]]
+];
